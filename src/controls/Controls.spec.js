840bc5ff473a25c5e47abad5d6dd94ba228fd96e
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-04 10:14:01 -0600

    controls and display tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import Controls from "./Controls"
+
+test('default has disabled "Lock Gate" and enabled "Close Gate" button', () => {
+    const wrapper = rtl.render(<Controls locked={false} closed={false} toggleLocked={()=>{}} toggleClosed={()=>{}}/>)
+
+    expect(wrapper.queryByText("Lock Gate").disabled).toBe(true)
+    expect(wrapper.queryByText("Close Gate").disabled).toBe(false)
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import Display from "./Display"
+
+test('default state is open and unlocked', ()=> {
+    const wrapper = rtl.render(<Display locked={false} closed={false}/>)
+
+    expect(wrapper.queryByText("Open")).toBeTruthy();
+    expect(wrapper.queryByText("Closed")).toBeNull();
+    
+})
