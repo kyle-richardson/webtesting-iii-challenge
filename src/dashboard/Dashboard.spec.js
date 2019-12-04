a336115ea7ead6f4e6d5683024fa71b8ac23e972
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-03 23:21:06 -0600

    dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,36 @@
 // Test away
+
+import React from "react";
+import * as rtl from "@testing-library/react";
+import Dashboard from "./Dashboard"
+
+test ('open button renders open text', () => {
+    const wrapper = rtl.render(<Dashboard/>)
+
+  
+    const Open= wrapper.getByText(/open/i)
+
+    rtl.act(()=> {
+        rtl.fireEvent.click(Open);
+    })
+
+    expect(wrapper.queryByText("Open")).toBeTruthy();
+    expect(wrapper.queryByText("Closed")).toBeNull();
+    expect(wrapper.queryByText("Lock Gate").disabled).toBe(true)
+    expect(wrapper.queryByText("Close Gate").disabled).toBe(false)
+})
+
+test ('close button renders closed text', () => {
+    const wrapper = rtl.render(<Dashboard/>)
+
+  
+    const Close= wrapper.getByText(/close/i)
+
+    rtl.act(()=> {
+        rtl.fireEvent.click(Close);
+    })
+
+    expect(wrapper.queryByText("Open")).toBeNull();
+    expect(wrapper.queryByText("Closed")).toBeTruthy();
+})
+
