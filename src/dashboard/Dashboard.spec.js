73e2e0e7f90650a4aa72e2ad4b9cac3495da6bd1
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-04 09:57:20 -0600

    more tests for open and closed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,40 @@
-// Test away
-
 import React from "react";
 import * as rtl from "@testing-library/react";
 import Dashboard from "./Dashboard"
 
-test ('open button renders open text', () => {
+test('default state is open and unlocked', ()=> {
     const wrapper = rtl.render(<Dashboard/>)
 
-  
-    const Open= wrapper.getByText(/open/i)
+    expect(wrapper.queryByText("Open")).toBeTruthy();
+    expect(wrapper.queryByText("Closed")).toBeNull();
+    expect(wrapper.queryByText("Lock Gate").disabled).toBe(true)
+    expect(wrapper.queryByText("Close Gate").disabled).toBe(false)
+})
 
+test('close button renders closed text', () => {
+    const wrapper = rtl.render(<Dashboard/>)
+
+    const Close= wrapper.getByText("Close Gate")
+
+    rtl.act(()=> {
+        rtl.fireEvent.click(Close);
+    })
+
+    expect(wrapper.queryByText("Open")).toBeNull();
+    expect(wrapper.queryByText("Closed")).toBeTruthy();
+    expect(wrapper.queryByText("Lock Gate").disabled).toBe(false)
+    expect(wrapper.queryByText("Open Gate").disabled).toBe(false)
+})
+
+test('open button renders open text', () => {
+    const wrapper = rtl.render(<Dashboard/>)
+    
+    const Close= wrapper.getByText("Close Gate")
+    rtl.act(()=> {
+        rtl.fireEvent.click(Close);
+    })
+
+    const Open= wrapper.getByText("Open Gate")
     rtl.act(()=> {
         rtl.fireEvent.click(Open);
     })
@@ -20,17 +45,55 @@ test ('open button renders open text', () => {
     expect(wrapper.queryByText("Close Gate").disabled).toBe(false)
 })
 
-test ('close button renders closed text', () => {
+
+
+test('lock button renders locked text', () => {
     const wrapper = rtl.render(<Dashboard/>)
 
   
-    const Close= wrapper.getByText(/close/i)
+    const Lock= wrapper.getByText("Lock Gate")
+    const Close= wrapper.getByText("Close Gate")
 
     rtl.act(()=> {
         rtl.fireEvent.click(Close);
     })
 
+    rtl.act(()=> {
+        rtl.fireEvent.click(Lock);
+    })
+
     expect(wrapper.queryByText("Open")).toBeNull();
     expect(wrapper.queryByText("Closed")).toBeTruthy();
+    expect(wrapper.queryByText("Locked")).toBeTruthy();
+    expect(wrapper.queryByText("Unlock Gate").disabled).toBe(false)
+    expect(wrapper.queryByText("Open Gate").disabled).toBe(true)
 })
+
+test('unlock button renders unlocked text', () => {
+    const wrapper = rtl.render(<Dashboard/>)
+
+  
+    
+    const Close= wrapper.getByText("Close Gate")
+    const Lock = wrapper.getByText("Lock Gate")
+
+    rtl.act(()=> {
+        rtl.fireEvent.click(Close);
+    })
+    rtl.act(()=> {
+        rtl.fireEvent.click(Lock);
+    })
+    const Unlock= wrapper.getByText("Unlock Gate")
+    rtl.act(()=> {
+        rtl.fireEvent.click(Unlock);
+    })
+
+    expect(wrapper.queryByText("Open")).toBeNull();
+    expect(wrapper.queryByText("Closed")).toBeTruthy();
+    expect(wrapper.queryByText("Unlocked")).toBeTruthy();
+    expect(wrapper.queryByText("Lock Gate").disabled).toBe(false)
+    expect(wrapper.queryByText("Open Gate").disabled).toBe(false)
+})
+
+
 
